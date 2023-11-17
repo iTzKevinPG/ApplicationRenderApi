fa7fda88e090bfc10c89c84bada7d6e300a865c3
KevinUJ <kevin.useche@pirani.co>
2023-11-17 13:40:17 -0500

    add status

--- userapp/views.py
+++ userapp/views.py
@@ -20,7 +20,7 @@ def addUser(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()   
-        return Response(serializer.data, status=201) 
+        return Response(serializer.data, status=200) 
 
     return Response(serializer.errors, status=400) 
 
